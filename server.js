37ad344fc820704e967dabc2588dd71c97698a03
yankiweiss <yakovw2706@gmail.com>
2025-07-15 14:33:31 -0400

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'HTML', 'index.html'));
 });
 
 app.listen(PORT, () => {
